de6d1b91256ab0d03234d17501d754f046ec54fb
SergeiT <uraniumcore238@gmail.com>
2022-10-05 10:28:18 +0300

    completed test_desktop_visibility_header_elements

--- tests/test_header_elements_visibility.py
+++ tests/test_header_elements_visibility.py
@@ -1,8 +1,7 @@
 import os
-import time
 import allure
-from selene import be
 
+from pages.base_page import BasePage
 from pages.main_page import MainPage
 
 
@@ -14,20 +13,18 @@ class TestHeaderElementsVisibility:
 
         with allure.step('Open page discord.com'):
             browser.open(url)
-
         with allure.step('Assert logo visibility'):
-            MainPage.logo.should(be.visible)
-            time.sleep(10)
-        # with allure.step('Assert text in element'):
-        #     BasePage.assert_exact_text_in_element(self, MainPage.download, 'Download')
-        # with allure.step('Assert texts in elements'):
-        #     BasePage.assert_exact_text_in_element(self, MainPage.nitro, 'Nitro')
-        #     BasePage.assert_exact_text_in_element(self, MainPage.discover, 'Discover')
-        #     BasePage.assert_exact_text_in_element(self, MainPage.safety, 'Safety')
-        #     BasePage.assert_exact_text_in_element(self, MainPage.support, 'Support')
-        #     BasePage.assert_exact_text_in_element(self, MainPage.blog, 'Blog')
-        #     BasePage.assert_exact_text_in_element(self, MainPage.careers, 'Careers')
-        # with allure.step('Assert login button visibility'):
-        #     BasePage.assert_element_visibility(self, MainPage.login_btn)
+            BasePage.assert_element_visibility(self, MainPage.logo)
+        with allure.step('Assert text in element'):
+            BasePage.assert_exact_text_in_element(self, MainPage.download, 'Download')
+        with allure.step('Assert texts in elements'):
+            BasePage.assert_exact_text_in_element(self, MainPage.nitro, 'Nitro')
+            BasePage.assert_exact_text_in_element(self, MainPage.discover, 'Discover')
+            BasePage.assert_exact_text_in_element(self, MainPage.safety, 'Safety')
+            BasePage.assert_exact_text_in_element(self, MainPage.support, 'Support')
+            BasePage.assert_exact_text_in_element(self, MainPage.blog, 'Blog')
+            BasePage.assert_exact_text_in_element(self, MainPage.careers, 'Careers')
+        with allure.step('Assert login button visibility'):
+            BasePage.assert_element_visibility(self, MainPage.login_btn)
 
 
