e80fa2c1754f026ec5e2fa58ac3418ace1100200
SergeiT <uraniumcore238@gmail.com>
2022-10-05 12:49:50 +0300

    completed mobile_desktop_visibility_header_elements

--- tests/test_header_elements_visibility.py
+++ tests/test_header_elements_visibility.py
@@ -1,4 +1,5 @@
 import os
+
 import allure
 
 from pages.base_page import BasePage
@@ -7,6 +8,9 @@ from pages.main_page import MainPage
 
 class TestHeaderElementsVisibility:
 
+    @allure.severity('Blocker')
+    @allure.label("owner", "uraniumcore238")
+    @allure.story('Visibility elements on discord.com page')
     def test_desktop_visibility_header_elements(self, setup_browser):
         browser = setup_browser
         url = os.getenv('main_url')
@@ -27,4 +31,32 @@ class TestHeaderElementsVisibility:
         with allure.step('Assert login button visibility'):
             BasePage.assert_element_visibility(self, MainPage.login_btn)
 
+    @allure.severity('Blocker')
+    @allure.label("owner", "uraniumcore238")
+    @allure.story('Visibility elements on discord.com page')
+    def test_mobile_header_visibility_elements(self, setup_mobile_browser):
+        browser = setup_mobile_browser
+        url = os.getenv('main_url')
 
+        with allure.step('Open page discord.com'):
+            browser.open(url)
+        with allure.step('Assert mobile logo visibility'):
+            BasePage.assert_element_visibility(self, MainPage.mobile_logo)
+        with allure.step('Assert mobile login button visibility'):
+            BasePage.assert_element_visibility(self, MainPage.mobile_login_btn)
+        with allure.step('Click on side menu button'):
+            BasePage.click_on_element(self, MainPage.side_menu_button)
+        with allure.step('Assert side menu visibility'):
+            BasePage.assert_element_visibility(self, MainPage.side_menu)
+        with allure.step('Assert visibility side menu elements'):
+            BasePage.assert_exact_text_in_element(self, MainPage.home_side_menu, 'Home')
+            BasePage.assert_exact_text_in_element(self, MainPage.download_side_menu, 'Download')
+            BasePage.assert_exact_text_in_element(self, MainPage.nitro_side_menu, 'Nitro')
+            BasePage.assert_exact_text_in_element(self, MainPage.discover_side_menu, 'Discover')
+            BasePage.assert_exact_text_in_element(self, MainPage.safety_side_menu, 'Safety')
+            BasePage.assert_exact_text_in_element(self, MainPage.mod_academy_side_menu, 'Mod Academy')
+            BasePage.assert_exact_text_in_element(self, MainPage.support_side_menu, 'Support')
+            BasePage.assert_exact_text_in_element(self, MainPage.blog_side_menu, 'Blog')
+            BasePage.assert_exact_text_in_element(self, MainPage.careers_side_menu, 'Careers')
+        with allure.step('Assert download button visibility'):
+            BasePage.assert_element_visibility(self, MainPage.download_button)
