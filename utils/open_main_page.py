d8533a21513b88c43ba7cf0bf7781517cbd01edb
SergeiT <uraniumcore238@gmail.com>
2022-10-12 10:54:36 +0300

    make tests more atomic

--- utils/open_main_page.py
+++ utils/open_main_page.py
@@ -1,8 +1,6 @@
 import os
 import allure
 import pytest
-from pages.base_page import BasePage
-from pages.main_page import MainPage
 
 
 @pytest.fixture(scope='class')
@@ -11,19 +9,3 @@ def open_main_page(setup_browser):
     url = os.getenv('main_url')
     with allure.step('Open page discord.com'):
         browser.open(url)
-
-
-# @pytest.fixture(scope='class')
-# def open_mobile_main_page(setup_mobile_browser):
-#     browser = setup_mobile_browser
-#     url = os.getenv('main_url')
-#     with allure.step('Open page discord.com'):
-#         browser.open(url)
-#
-#
-# @pytest.fixture(scope='function')
-# def open_side_menu():
-#     with allure.step('Click on side menu button'):
-#         BasePage().click_on_element(MainPage.side_menu_button)
-#     with allure.step('Assert side menu visibility'):
-#         BasePage().assert_element_visibility(MainPage.side_menu)
