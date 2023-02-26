ecea0bbfb96c514638325813657741007d6771b8
SergeiT <uraniumcore238@gmail.com>
2023-02-26 19:09:05 +0200

    run api tests only

--- tests/api/test_get_location_metadata.py
+++ tests/api/test_get_location_metadata.py
@@ -1,11 +1,15 @@
+import allure
+
 from schemas.location_metadata import location_metadata
 from utils.sessions import api_url
 from pytest_voluptuous import S
 
 
+@allure.title('Api test')
 def test_get_location_metadata():
-    response = api_url().get("/auth/location-metadata")
-    assert response.status_code == 200
-    assert S(location_metadata) == response.json()
+    with allure.step('Send request'):
+        response = api_url().get("/auth/location-metadata")
+        assert response.status_code == 200
+        assert S(location_metadata) == response.json()
 
 
